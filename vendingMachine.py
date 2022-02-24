2f516098fc77be2e994e5563bf73574680327695
Shubham <s.shrikhande1@gmail.com>
2022-02-24 22:22:14 +0530

    [Shubham]Added: this file to get the change of currency

--- vendingMachine.py
+++ vendingMachine.py
@@ -1,9 +1,16 @@
-def notes():
-    money = [1, 2, 5, 10, 50, 100, 200, 500, 2000]
-    amount = int(input("Enter amount"))
-    for i in range(1, 8):
-        amount = amount / money[i]
-    return amount
+def count_currency(amount):
+    notes = [2000, 500, 200, 100,
+             50, 20, 10, 5, 1]
+
+    print("****Currency Count****")
+
+    for i in notes:
+        if amount >= i:
+            j = amount // i
+            amount = amount - j * i
+            print(i, " : ", j)
 
 
-print(notes())
+# Calling function
+amount = int(input("Enter the amount :"))
+count_currency(amount)
